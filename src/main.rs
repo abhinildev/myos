fb22c12abfb4219c2e8e8ccbf288103aed09c3e1
abhinildev <savarniabhinil@gmail.com>
2025-09-26 20:31:42 +0530

    post 4 done

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,77 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main="test_main"]
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
-
+//use myos::println;
+mod vga_buffer;
+mod serial;
+#[test_case]
+fn trivial_assertion(){
+    // print!("trivial assertion ....");
+    // assert_eq!(1,1);
+    // println!("[ok]");
+    //serial_print!("trivial assertion... ");
+    assert_eq!(1,1);
+    //serial_println!("[ok]");
+}
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]){
+    //println!("Running {} tests",tests.len());
+    serial_print!("Running {} tests",tests.len());
+    for test in tests{
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Sucess);
+}
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info:&PanicInfo)->! {
+fn panic(info: &PanicInfo)-> ! {
+    println!("{}", info);
     loop {}
 }
-static HELLO: &[u8]=b"Hello OS from os!!";
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> !{
+    myos::test_panic_handler(info)
+}
+//static HELLO: &[u8]=b"Hello OS from os!!";
 #[unsafe(no_mangle)]
 pub extern "C" fn _start()-> !{
-    let vga_buffer=0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate(){
-        unsafe {
-            *vga_buffer.offset(i as isize * 2)= byte;
-            *vga_buffer.offset(i as isize * 2 +1)=0xb;
-        }
+//    use core::fmt::Write;
+//    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+//    write!(vga_buffer::WRITER.lock(),", some numbers: {} {}",42,1.337).unwrap();
+   println!("Hello World{}","!");
+   //panic!("Some panic message");
+   #[cfg(test)]
+   test_main();
+   loop{}
+}
+#[derive(Debug,Clone,Copy,PartialEq,Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Sucess=0x10,
+    Failed=0x11,
+}
+pub fn exit_qemu(exit_code:QemuExitCode){
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port =Port::new(0xf4);
+        port.write(exit_code as u32);
     }
-    loop{}
+}
+pub trait Testable {
+    fn run(&self)->();
+}
+impl <T> Testable for T
+where 
+    T:Fn(),
+{
+    fn run(&self){
+        serial_print!("{}...\t",core::any::type_name::<T>());
+        self();
+        serial_print!("[ok]");
+    }
+    
 }
---
+++ tests/basic_boot.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main="test_main"]
+use myos::println;
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop{}
+}
+fn test_runner(_tests: &[&dyn Fn()]){
+    println!("Basic boot test ran")
+}
+#[panic_handler]
+fn panic(info: &PanicInfo)-> !{
+    myos::test_panic_handler(info)
+}
+#[test_case]
+fn test_println(){
+  println!("Test println output");
+}
