9a7b67d767a0ce70858b09adb5882efcf0beace3
abhinildev <savarniabhinil@gmail.com>
2025-12-23 13:15:47 +0530

    Part 1 done

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,78 @@
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main="test_main"]
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(myos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
+use myos::println;
+use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-//use myos::println;
-mod vga_buffer;
-mod serial;
-#[test_case]
-fn trivial_assertion(){
-    // print!("trivial assertion ....");
-    // assert_eq!(1,1);
-    // println!("[ok]");
-    //serial_print!("trivial assertion... ");
-    assert_eq!(1,1);
-    //serial_println!("[ok]");
-}
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]){
-    //println!("Running {} tests",tests.len());
-    serial_print!("Running {} tests",tests.len());
-    for test in tests{
-        test.run();
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use myos::allocator;
+    use myos::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+
+    println!("Hello World{}", "!");
+    myos::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset.into());
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
     }
-    exit_qemu(QemuExitCode::Sucess);
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    myos::hlt_loop();
 }
+
+/// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
-fn panic(info: &PanicInfo)-> ! {
+fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     myos::hlt_loop();
 }
+
 #[cfg(test)]
 #[panic_handler]
-fn panic(info: &PanicInfo) -> !{
+fn panic(info: &PanicInfo) -> ! {
     myos::test_panic_handler(info)
 }
-//static HELLO: &[u8]=b"Hello OS from os!!";
-#[unsafe(no_mangle)]
-pub extern "C" fn _start()-> !{
-//    use core::fmt::Write;
-//    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-//    write!(vga_buffer::WRITER.lock(),", some numbers: {} {}",42,1.337).unwrap();
-    myos::init();
-    // unsafe {
-    //     *(0xdeadbeef as *mut u8)=42;
-    // };
-    //x86_64::instructions::interrupts::int3();
-    
-    //stack_overflow();
-   println!("Hello World{}","!");
-   //panic!("Some panic message");
-   #[cfg(test)]
-   test_main();
 
-   println!("It did not crash");
-   myos::hlt_loop();
-}
-#[derive(Debug,Clone,Copy,PartialEq,Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Sucess=0x10,
-    Failed=0x11,
-}
-pub fn exit_qemu(exit_code:QemuExitCode){
-    use x86_64::instructions::port::Port;
-    unsafe {
-        let mut port =Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-pub trait Testable {
-    fn run(&self)->();
-}
-impl <T> Testable for T
-where 
-    T:Fn(),
-{
-    fn run(&self){
-        serial_print!("{}...\t",core::any::type_name::<T>());
-        self();
-        serial_print!("[ok]");
-    }
-    
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
